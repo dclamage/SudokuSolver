f5d66687ee58f63613dc2f0ef3be93815b0cbbb4
WaffeFIN <waltergronholm@gmail.com>
2022-03-30 15:06:35 +0300

    GreaterSumConstraint now uses SumCellsHelper for more accurate sums

--- SudokuSolver/Constraints/GreaterSumConstraint.cs
+++ SudokuSolver/Constraints/GreaterSumConstraint.cs
@@ -6,15 +6,18 @@ using static SudokuSolver.SolverUtility;
 
 namespace SudokuSolver.Constraints
 {
+
     [Constraint(DisplayName = "Greater Sum", ConsoleName = "gtsum")]
     public class GreaterSumConstraint : Constraint
     {
         public readonly List<(int, int)> greatCells;
         public readonly List<(int, int)> smallCells;
-        private readonly HashSet<(int, int)> allCells;
-        private bool ok;
+        private readonly SumCellsHelper greatCellsSumHelper;
+        private readonly SumCellsHelper smallCellsSumHelper;
 
-        public GreaterSumConstraint(Solver sudokuSolver, string options) : base(sudokuSolver)
+        private readonly HashSet<(int, int)> allCells;
+
+        public GreaterSumConstraint(Solver solver, string options) : base(solver)
         {
             var cellGroups = ParseCells(options);
             if (cellGroups.Count != 2)
@@ -24,141 +27,82 @@ namespace SudokuSolver.Constraints
 
             greatCells = cellGroups[0];
             smallCells = cellGroups[1];
+            greatCellsSumHelper = new(solver, greatCells);
+            smallCellsSumHelper = new(solver, smallCells);
+
             allCells = new(greatCells.Concat(smallCells));
         }
 
         public override string SpecificName => $"Greater sum {CellName(greatCells[0])} > {CellName(smallCells[0])}";
 
-        public override LogicResult InitCandidates(Solver sudokuSolver)
+        public override LogicResult InitCandidates(Solver solver)
         {
-            ok = false;
-            return IsValid(sudokuSolver) ? LogicResult.None : LogicResult.Invalid;
+            var greatResult = greatCellsSumHelper.Init(solver, AtLeast(greatCells.Count, greatCells.Count, MAX_VALUE));
+            var smallResult = smallCellsSumHelper.Init(solver, AtLeast(smallCells.Count, smallCells.Count, MAX_VALUE));
+
+            return !IsValid(solver) ? LogicResult.Invalid : greatResult > smallResult ? greatResult : smallResult;
         }
 
-        private bool IsValid(Solver sudokuSolver)
+        private bool IsValid(Solver solver)
         {
-            return ok || MaxSumCells(sudokuSolver, greatCells) > MinSumCells(sudokuSolver, smallCells);
+            var maxGreat = MaxGreat(solver);
+            var minSmall = MinSmall(solver);
+            return maxGreat != 0 && minSmall != 0 && maxGreat > minSmall;
         }
 
-        private static int MaxSumCells(Solver solver, List<(int, int)> cells)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private int MinSmall(Solver solver)
         {
-            var sum = 0;
-            foreach ((int, int) cell in cells)
-            {
-                sum += SolverUtility.MaxValue(solver.Board[cell.Item1, cell.Item2]);
-            }
-            return sum;
+            return smallCellsSumHelper.SumRange(solver).Item1;
         }
 
-        private static int MinSumCells(Solver solver, List<(int, int)> cells)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private int MaxGreat(Solver solver)
         {
-            var sum = 0;
-            foreach ((int, int) cell in cells)
-            {
-                sum += SolverUtility.MinValue(solver.Board[cell.Item1, cell.Item2]);
-            }
-            return sum;
+            return greatCellsSumHelper.SumRange(solver).Item2;
         }
 
         public override bool EnforceConstraint(Solver sudokuSolver, int i, int j, int val)
         {
-            return ok || !allCells.Contains((i, j)) || IsValid(sudokuSolver);
+            return !allCells.Contains((i, j)) || IsValid(sudokuSolver);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static IEnumerable<int> AtMost(int maxValue, int nCells)
+        {
+            return Enumerable.Range(nCells, maxValue - nCells + 1);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static IEnumerable<int> AtLeast(int minValue, int nCells, int MAX_VALUE)
+        {
+            return Enumerable.Range(minValue, nCells * MAX_VALUE - minValue + 1);
         }
 
         public override LogicResult StepLogic(Solver solver, StringBuilder logicalStepDescription, bool isBruteForcing)
         {
-            if (ok)
-            {
-                return LogicResult.None;
-            }
-
-            var minSum = MinSumCells(solver, smallCells);
-            var maxSum = MaxSumCells(solver, greatCells);
+            var minSum = MinSmall(solver);
+            var maxSum = MaxGreat(solver);
 
             if (maxSum <= minSum)
             {
-                logicalStepDescription?.Append($"Sum of cells at {CellName(greatCells[0])} is not greater than the sum of cells at {CellName(smallCells[0])}");
+                logicalStepDescription?.Append($"Sum of cells at {CellName(greatCells[0])} can not be greater than the sum of cells at {CellName(smallCells[0])}");
                 return LogicResult.Invalid;
             }
 
             if (CellsFilled(solver, greatCells) && CellsFilled(solver, smallCells))
             {
-                ok = true;
                 return LogicResult.None;
             }
 
-            if (maxSum > minSum + solver.MAX_VALUE - 1) // Sums are not close enough to eliminate candidates, so chill
+            var greatSumResult = greatCellsSumHelper.StepLogic(solver, AtLeast(minSum, greatCells.Count, MAX_VALUE), logicalStepDescription);
+            if (greatSumResult == LogicResult.Invalid)
             {
-                return LogicResult.None;
+                return LogicResult.Invalid;
             }
 
-            // Simple candidate removal
-            var smallResult = DifferenceToMinCandidatesMustBeLessThan(solver, smallCells, maxSum - minSum);
-            var greatResult = DifferenceToMaxCandidatesMustBeLessThan(solver, greatCells, maxSum - minSum);
-
-            return smallResult > greatResult ? smallResult : greatResult;
-        }
-
-        private static LogicResult DifferenceToMinCandidatesMustBeLessThan(Solver solver, List<(int, int)> cells, int diff)
-        {
-            var changed = false;
-            foreach ((int, int) cell in cells)
-            {
-                var mask = solver.Board[cell.Item1, cell.Item2];
-                if (SolverUtility.IsValueSet(mask))
-                {
-                    continue;
-                }
-
-                var minValue = SolverUtility.MinValue(mask);
-                // Remove all candidates larger than minValue + diff
-                // 0000...0011...1
-                //          ^^^^^^-- (minValue + diff + 1) ones, so minValue + diff is the highest allowed candidate
-                var newMask = mask & ((1 << (minValue + diff + 1)) - 1);
-                
-                if (newMask == 0)
-                {
-                    return LogicResult.Invalid;
-                }
-
-                if (newMask != mask)
-                {
-                    solver.Board[cell.Item1, cell.Item2] = mask;
-                    changed = true;
-                }
-            }
-            return changed ? LogicResult.Changed : LogicResult.None;
-        }
-
-        private static LogicResult DifferenceToMaxCandidatesMustBeLessThan(Solver solver, List<(int, int)> cells, int diff)
-        {
-            var changed = false;
-            foreach ((int, int) cell in cells)
-            {
-                var mask = solver.Board[cell.Item1, cell.Item2];
-                if (SolverUtility.IsValueSet(mask))
-                {
-                    continue;
-                }
-
-                var maxValue = SolverUtility.MaxValue(mask);
-                // Remove all candidates smaller than maxValue - diff
-                // 1111...1100...0
-                //          ^^^^^^-- (maxValue - diff - 1) zeroes, so maxValue - diff is the lowest allowed candidate
-                var newMask = mask & ~((1 << (maxValue - diff - 1)) - 1); 
-
-                if (newMask == 0)
-                {
-                    return LogicResult.Invalid;
-                }
-
-                if (newMask != mask)
-                {
-                    solver.Board[cell.Item1, cell.Item2] = mask;
-                    changed = true;
-                }
-            }
-            return changed ? LogicResult.Changed : LogicResult.None;
+            var smallSumResult = smallCellsSumHelper.StepLogic(solver, AtMost(maxSum, smallCells.Count), logicalStepDescription);
+            return smallSumResult > greatSumResult ? smallSumResult : greatSumResult;
         }
 
         private static bool CellsFilled(Solver solver, List<(int, int)> cells)
