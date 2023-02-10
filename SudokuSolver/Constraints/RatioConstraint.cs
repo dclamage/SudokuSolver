0541a73673817239bad2dd1e4a33f6aa242002f1
yusitnikov <yuri.sitnikov@kaltura.com>
2023-02-10 21:13:37 +0200

    Split kropki dots and XV into individual constraint objects

--- SudokuSolver/Constraints/RatioConstraint.cs
+++ SudokuSolver/Constraints/RatioConstraint.cs
@@ -7,6 +7,14 @@ public class RatioConstraint : OrthogonalValueConstraint
     {
     }
 
+    public RatioConstraint(Solver sudokuSolver, int negativeConstraintValue) : base(sudokuSolver, negativeConstraintValue)
+    {
+    }
+
+    public RatioConstraint(Solver sudokuSolver, int markerValue, (int, int) cell1, (int, int) cell2) : base(sudokuSolver, markerValue, cell1, cell2)
+    {
+    }
+
     protected override bool IsPairAllowedAcrossMarker(int markerValue, int v0, int v1) => (v0 * markerValue == v1 || v1 * markerValue == v0);
 
     protected override IEnumerable<OrthogonalValueConstraint> GetRelatedConstraints(Solver solver) =>
