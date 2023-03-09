3f997b75e1f611c74e729fb0a39296ef91ece9f0
yusitnikov <yuri.sitnikov@kaltura.com>
2023-03-09 20:59:42 +0200

    Fix NullReferenceException in OrthogonalValueConstraint
    by replacing class constructor reflection with abstract methods

--- SudokuSolver/Constraints/SumConstraint.cs
+++ SudokuSolver/Constraints/SumConstraint.cs
@@ -15,6 +15,16 @@ public class SumConstraint : OrthogonalValueConstraint
     {
     }
 
+    protected override OrthogonalValueConstraint createNegativeConstraint(Solver sudokuSolver, int negativeConstraintValue)
+    {
+        return new SumConstraint(sudokuSolver, negativeConstraintValue);
+    }
+
+    protected override OrthogonalValueConstraint createMarkerConstraint(Solver sudokuSolver, int markerValue, (int, int) cell1, (int, int) cell2)
+    {
+        return new SumConstraint(sudokuSolver, markerValue, cell1, cell2);
+    }
+
     protected override bool IsPairAllowedAcrossMarker(int markerValue, int v0, int v1) => (v0 + v1 == markerValue);
 
     protected override int DefaultMarkerValue => 5;
