a236cecf988e87c5ebd5f0a376394a40addead34
David Clamage <rangsk@gmail.com>
2025-11-29 05:57:17 -0700

    Version 1.3.2

--- UserScripts/Fpuzzles-SudokuSolver.user.js
+++ UserScripts/Fpuzzles-SudokuSolver.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fpuzzles-SudokuSolver
 // @namespace    http://tampermonkey.net/
-// @version      1.3.1
+// @version      1.3.2
 // @description  Connect f-puzzles to SudokuSolver
 // @author       Rangsk
 // @match        https://*.f-puzzles.com/*
